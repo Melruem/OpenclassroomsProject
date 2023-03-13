8336f65e0f409fab29dd046283612d7a14c7344d
melruem <marcellin15kenfack@yahoo.fr>
2023-03-13 17:19:40 +0100

    premier programme laravel

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Game;
+
+class GameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $games = Game::all();
+        
+        return view('index',compact('games'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'price' => 'required',
+        ]);
+        $show = Game::create($validatedData);
+   
+        return redirect('/games')->with('success', 'Game is successfully saved');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $game = Game::findOrFail($id);
+
+        return view('edit', compact('game'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'price' => 'required'
+        ]);
+        Game::whereId($id)->update($validatedData);
+
+        return redirect('/games')->with('success', 'Game Data is successfully updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $game = Game::findOrFail($id);
+        $game->delete();
+
+        return redirect('/games')->with('success', 'Game Data is successfully deleted');
+    }
+}
